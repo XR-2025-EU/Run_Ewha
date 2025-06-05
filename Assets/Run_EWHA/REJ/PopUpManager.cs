a929917beca95ea88a04930f7f65b10dd03e6a08
heryoo920 <heryoo@naver.com>
2025-06-06 00:36:58 +0900

    0606_update

--- Assets/Run_EWHA/REJ/PopUpManager.cs
+++ Assets/Run_EWHA/REJ/PopUpManager.cs
@@ -5,11 +5,14 @@ public class PopupManager : MonoBehaviour
 {
     public GameObject popupPanelECC;  // Panel 전체
     public GameObject popupPanelPosco;
+    public GameObject popupPanelSH;
     private bool isPop = false;
 
     void Start()
     {
         popupPanelECC.SetActive(false);
+        popupPanelPosco.SetActive(false);
+        popupPanelSH.SetActive(false);
     }
 
     public void ShowPopup()
@@ -40,6 +43,21 @@ public class PopupManager : MonoBehaviour
                 popupPanelPosco.SetActive(false);
                 isPop = false;
             }
+        }
+
+        else if (Select_Timetable.Instance.selectedOption == 3)
+        {
+            if (!isPop)
+            {
+                popupPanelSH.SetActive(true);
+                isPop = true;
+            }
+            else
+            {
+                popupPanelSH.SetActive(false);
+                isPop = false;
+            }
+
         }
     }
 }
