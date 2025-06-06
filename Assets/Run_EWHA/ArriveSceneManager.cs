23bc5aa0f8d737b084eb7cb76b6ff2bd6a34d971
minha222 <minha2469@ewhain.net>
2025-06-06 22:50:29 +0900

    타이머 관련 스크립트 수정

--- Assets/Run_EWHA/ArriveSceneManager.cs
+++ Assets/Run_EWHA/ArriveSceneManager.cs
@@ -13,14 +13,13 @@ public class ArriveSceneManager : MonoBehaviour
             int minutes = (int)totalSeconds / 60;
             int seconds = (int)totalSeconds % 60;
 
-            arriveTimeText.text = $"소요 시간: {minutes:D2}m {seconds:D2}s";
+            arriveTimeText.text = $"{minutes:D2}m {seconds:D2}s";
             Debug.Log($"도착 씬에서 받은 시간: {minutes:D2}m {seconds:D2}s");
         }
         else
         {
-            arriveTimeText.text = "소요 시간: 정보 없음";
+            arriveTimeText.text = "정보 없음";
             Debug.Log("저장된 이동시간이 없습니다!");
         }
     }
-
 }
